fbea48406d54a43843a4fdb4ff92dc16de6a916d
Nirlep5252 <nirlep5252@proton.me>
2023-03-28 08:50:48 +0530

    committing before i go wild and ruin all of it

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -3,90 +3,34 @@ use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-const KEYWORDS: [&str; 1] = ["print"];
-const OPENING: [char; 4] = ['[', '(', '<', '{'];
-const CLOSING: [char; 4] = [']', ')', '>', '}'];
+const KEYWORDS: [&str; 2] = ["print", "ignorethis"];
+const OPENING: [char; 3] = ['[', '(', '{'];
+const CLOSING: [char; 3] = [']', ')', '}'];
+const OPERATORS: [char; 10] = ['+', '-', '*', '/', '^', '&', '|', '!', '>', '<'];
+// TODO: return statement's name => fuckoff
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     Keyword,
     String,
     Opening,
     Closing,
+    Identifier,
+    Operator,
+    Seperator,
+    Assignment,
 }
 
-#[derive(Debug)]
-#[allow(unused)]
+#[derive(Debug, PartialEq, Clone)]
 struct Token {
     typ: TokenType,
     val: String,
 }
 
-fn is_string(token: &str) -> bool {
-    (token.starts_with("\"") && token.ends_with("'") && !token.ends_with("\\'"))
-        || (token.starts_with("'") && token.ends_with("\"") && !token.ends_with("\\\""))
-}
-
-fn tokenize(code: String) -> Vec<Token> {
-    let mut tokens = vec![];
-    let mut temp = String::new();
-
-    fn token_type(tk: &str) -> Result<TokenType, ()> {
-        if KEYWORDS.contains(&tk) {
-            Ok(TokenType::Keyword)
-        } else if is_string(tk) {
-            Ok(TokenType::String)
-        } else {
-            Err(())
-        }
-    }
-
-    // TODO: reverse tokenization
-    for line in code.split("\n") {
-        for ch in line.chars() {
-            // dbg!(&tokens, &ch, &temp);
-            if ch == ' ' {
-                if temp.starts_with("\"") || temp.starts_with("'") {
-                    temp.push(ch);
-                    continue;
-                }
-                if temp == "" {
-                    continue;
-                }
-                match token_type(temp.as_str()) {
-                    Ok(t) => tokens.push(Token {
-                        typ: t,
-                        val: temp.clone(),
-                    }),
-                    Err(_) => error(format!("`{temp}` is not identified.")),
-                };
-                temp = "".to_string();
-            } else if OPENING.contains(&ch) || CLOSING.contains(&ch) {
-                if temp != "" {
-                    match token_type(temp.as_str()) {
-                        Ok(t) => tokens.push(Token {
-                            typ: t,
-                            val: temp.clone(),
-                        }),
-                        Err(_) => error(format!("`{temp}` is not identified.")),
-                    };
-                    temp = "".to_string();
-                }
-                tokens.push(Token {
-                    typ: if OPENING.contains(&ch) {
-                        TokenType::Opening
-                    } else {
-                        TokenType::Closing
-                    },
-                    val: String::from(format!("{ch}")),
-                });
-            } else {
-                // letters
-                temp.push(ch);
-            }
-        }
-    }
-    tokens
+#[derive(Debug)]
+struct Node {
+    children: Vec<Node>,
+    val: Option<Token>,
 }
 
 pub fn interpret(filename: &str) {
@@ -103,9 +47,161 @@ pub fn interpret(filename: &str) {
     match file.read_to_string(&mut s) {
         Err(e) => error(format!("ERROR: Unable to read: {filename}\n{e}")),
         Ok(_) => {
-            let tokens = tokenize(s);
-            dbg!(&tokens);
-            // TODO: actually interpret the fucking file lmfao
+            let lines = tokenize(s);
+            for line in lines {
+                let parse_tree = build_parse_tree(&line);
+            }
         }
     }
+}
+
+fn build_parse_tree(tokens: &Vec<Token>) -> Node {
+    let mut x = Node {
+        children: vec![],
+        val: None,
+    };
+    let assignment = Token {
+        typ: TokenType::Assignment,
+        val: "=".to_string(),
+    };
+
+    let mut has_operator = false;
+    let mut operator: Option<Token> = None;
+
+    for token in tokens {
+        if token.typ == TokenType::Operator {
+            has_operator = true;
+            operator = Some(token.clone());
+            break;
+        }
+    }
+
+    if tokens.len() == 1 {
+        x.val = Some(tokens[0].clone());
+    } else if tokens.contains(&assignment) {
+        // IGNORE PREVIOUS CODE
+        // TAKE 2 tokens from left and right of the operator and then uwu it 
+
+        // let ass_index = tokens.iter().position(|e| *e == assignment).unwrap();
+        // let left_tokens = &tokens[..ass_index].to_vec();
+        // let right_tokens = &tokens[ass_index + 1..].to_vec();
+        // x.children = vec![build_parse_tree(left_tokens), build_parse_tree(right_tokens)];
+        // x.val = Some(assignment);
+    } else if has_operator {
+        // let operator = operator.unwrap();
+        // let op_index = tokens.iter().position(|e| *e == operator).unwrap();
+        // let left_tokens = &tokens[..op_index].to_vec();
+        // let right_tokens = &tokens[op_index + 1..].to_vec();
+        // x.children = vec![build_parse_tree(left_tokens), build_parse_tree(right_tokens)];
+        // x.val = Some(operator);
+    } else {
+    }
+
+    x
+}
+
+fn tokenize(code: String) -> Vec<Vec<Token>> {
+    let mut tokens: Vec<Vec<Token>> = vec![];
+    let mut temp = String::new();
+
+    fn token_type(tk: &str) -> TokenType {
+        if KEYWORDS.contains(&tk) {
+            TokenType::Keyword
+        } else if is_string(tk) {
+            TokenType::String
+        } else {
+            TokenType::Identifier
+        }
+    }
+
+    fn is_comment(tkns: &Vec<Token>) -> bool {
+        tkns.last()
+            .unwrap_or(
+                &(Token {
+                    typ: TokenType::Keyword,
+                    val: "".to_string(),
+                }),
+            )
+            .val
+            == "ignorethis".to_string()
+    }
+
+    for line in code.split('\n').rev() {
+        let mut tokens_tmp: Vec<Token> = vec![];
+
+        fn push_curr_token(tokens_tmp: &mut Vec<Token>, temp: &mut String) {
+            tokens_tmp.push(Token {
+                typ: token_type(temp.as_str()),
+                val: temp.clone(),
+            });
+            *temp = "".to_string();
+        }
+
+        for ch in line.chars() {
+            if is_comment(&tokens_tmp) {
+                tokens_tmp.pop();
+                break;
+            }
+            if ch == ' ' {
+                if temp.starts_with("\"") || temp.starts_with("'") {
+                    temp.push(ch);
+                    continue;
+                }
+                if temp == "" {
+                    continue;
+                }
+                push_curr_token(&mut tokens_tmp, &mut temp);
+            } else if OPENING.contains(&ch) || CLOSING.contains(&ch) {
+                if temp != "" {
+                    push_curr_token(&mut tokens_tmp, &mut temp);
+                }
+                tokens_tmp.push(Token {
+                    typ: if OPENING.contains(&ch) {
+                        TokenType::Opening
+                    } else {
+                        TokenType::Closing
+                    },
+                    val: format!("{ch}"),
+                });
+            } else if OPERATORS.contains(&ch) {
+                if temp != "" {
+                    push_curr_token(&mut tokens_tmp, &mut temp);
+                }
+                tokens_tmp.push(Token {
+                    typ: TokenType::Operator,
+                    val: format!("{ch}"),
+                });
+            } else if ch == ',' {
+                if temp != "" {
+                    push_curr_token(&mut tokens_tmp, &mut temp);
+                }
+                tokens_tmp.push(Token {
+                    typ: TokenType::Seperator,
+                    val: format!("{ch}"),
+                });
+            } else if ch == '=' {
+                if temp != "" {
+                    push_curr_token(&mut tokens_tmp, &mut temp);
+                }
+                tokens_tmp.push(Token {
+                    typ: TokenType::Assignment,
+                    val: format!("{ch}"),
+                });
+            } else {
+                // letters
+                temp.push(ch);
+            }
+            // dbg!(&tokens, &temp, &ch);
+        }
+        if temp != "" {
+            push_curr_token(&mut tokens_tmp, &mut temp);
+        }
+        tokens.push(tokens_tmp);
+    }
+    tokens
+}
+
+fn is_string(token: &str) -> bool {
+    (token.starts_with("\"") && token.ends_with("'") && !token.ends_with("\\'"))
+        || (token.starts_with("'") && token.ends_with("\"") && !token.ends_with("\\\""))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use std::env;
+use utils::error;
 
 pub mod interpreter;
 pub mod utils;
 
 fn main() {
     let mut args = env::args();
-    // dbg!(&args);
 
     if args.len() == 1 {
-        println!("\nERROR: Expected filename.\nCorrect usage: `pain [filename]`");
+        error("\nERROR: Expected filename.\nCorrect usage: `pain [filename]`".to_string());
     } else {
-        interpreter::interpret(args.nth(1).unwrap().as_str());
+        let file = args.nth(1).unwrap();
+        if !file.ends_with(".pain") {
+            error("\nThe file should end with `.pain`.".to_string());
+        }
+        interpreter::interpret(file.as_str());
     }
 }
